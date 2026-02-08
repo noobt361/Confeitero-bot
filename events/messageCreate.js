03d50b4a3999b7dabd1776786b8fb48040d064db
zay <zay@email.com>
2026-02-08 14:05:51 -0300

    Mensagens atualização

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,16 +1,20 @@
 const randomReaction = require("../systems/randomReaction");
 const randomReply = require("../systems/randomReply");
 const mentionWatcher = require("../systems/mentionWatcher");
+const keywordResponder = require("../systems/keywordResponder");
 
 module.exports = {
   name: "messageCreate",
   async execute(client, message) {
     if (message.author.bot) return;
 
+    client.on("messageCreate", async (message) => {
+
     // sistemas passivos
     randomReaction(message, client);
     randomReply(message, client);
     mentionWatcher(message, client);
+    keywordResponder(message);
 
     // comandos
     if (!message.content.startsWith(client.PREFIX)) return;
