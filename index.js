dd104171f08b2ba41b3227ba13c0ea3839ba6023
zay <zay@email.com>
2026-02-08 09:17:21 -0300

    bot inicial limpo real2

--- index.js
+++ index.js
@@ -9,14 +9,3 @@ client.once("ready", () => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
-const { Client, GatewayIntentBits } = require("discord.js");
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-});
-
-client.once("ready", () => {
-  console.log(`Bot online como ${client.user.tag}`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
